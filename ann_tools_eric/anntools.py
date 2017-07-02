f8f3ade7a3711ebb27201273be32610804ff5bfd
Xiang Chen <simon.chan.per@gmail.com>
2017-07-02 14:21:11 +0200

    ver. beta 1702

--- ann_tools_eric/anntools.py
+++ ann_tools_eric/anntools.py
@@ -2,19 +2,68 @@
 """
 Module implementing MainWindow.
 """
-import os, sys
-
+import os, sys, math
+from time import sleep
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QRubberBand, QGraphicsScene, QFrame, QMainWindow, QFileDialog, QMessageBox, QDesktopWidget
-from PyQt5.QtGui import QPixmap, QPainter
-from PyQt5.QtCore import Qt, QPoint, QRect, QSize, pyqtSlot, QDir
+from PyQt5.QtWidgets import QRubberBand, QGraphicsScene, QFrame, QMainWindow, QFileDialog, QMessageBox, QDesktopWidget, QProgressBar
+from PyQt5.QtGui import QPixmap, QPainter, QColor, QBrush, QPen, QPalette
+from PyQt5.QtCore import Qt, QPoint, QRect, QSize, pyqtSlot, QDir, QStringListModel, QModelIndex, QThread
 
 from Ui_anntools import Ui_MainWindow
 from xml_process import xml_generator, xml_parsing
 from exif_info import get_info_from_image
 from comm_xml import insert_object, remove_all_object, object_isCheck, read_xml_object, conv_prop, rect_size_isChecked
 from PIL import Image
+from PyQt5.QtWidgets import QWidget
 
+class Overlay(QWidget):
+
+    def __init__(self, parent=None):
+
+        QWidget.__init__(self, parent)
+        
+        #self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_NoSystemBackground)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        #self.setAttribute(Qt.WA_PaintOnScreen)
+        #self.setAttribute(Qt.WA_TransparentForMouseEvents)
+        
+        palette = QPalette(self.palette())
+        palette.setColor(palette.Background, Qt.transparent)
+        self.setPalette(palette)
+
+    def paintEvent(self, event):
+
+        painter = QPainter()
+        painter.begin(self)
+        painter.setPen(QPen(Qt.NoPen))
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.fillRect(event.rect(), QBrush(QColor(255, 255, 255, 200)))
+
+        for i in range(6):
+            if (self.counter / 5) % 6 == i:
+                painter.setBrush(QBrush(QColor(120, 120, 120, 220)))
+
+            else:
+                painter.setBrush(QBrush(QColor(180, 180, 180, 220)))
+            painter.drawEllipse(self.width() / 2 + 30 * math.cos(2 * math.pi * i / 6.0) -
+                                    10, 
+                                self.height() / 2 + 30 * math.sin(2 * math.pi * i / 6.0) - 10, 
+                                20, 20)
+
+        #painter.end()
+
+    def showEvent(self, event):
+        self.timer = self.startTimer(50)
+        self.counter = 0
+
+    def timerEvent(self, event):
+        self.counter += 1
+        self.update()
+        if self.counter == 25:
+            self.killTimer(self.timer)
+            self.hide()
+        
 class MainWindow(QMainWindow, Ui_MainWindow):
     
     def __init__(self, parent=None):
@@ -29,7 +78,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         screenSize = QRect()
         desktop = QDesktopWidget()
         screenSize  = desktop.availableGeometry(self);
-        self.setFixedSize(screenSize.width(), screenSize.height());
+        
+        aps_ratio = 83/100 # set prefer ratio if you want 
+        
+        height = screenSize.height()*aps_ratio
+        print ('height',  height, screenSize.width(), screenSize.height())
+        self.setFixedSize(screenSize.width(), height);
         
         self.prevGraphicsView.setFrameShape(QFrame.NoFrame)
         self.currGraphicsView.setFrameShape(QFrame.NoFrame)
@@ -38,6 +92,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.currImgPath = ''
         self.prevXMLPath = ''
         self.currXMLPath = ''
+        self.prev_image_list = None
         
         self.prevScene = QGraphicsScene()
         self.prevGraphicsView.setScene(self.prevScene)
@@ -60,6 +115,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.currGraphicsView.mouseMoveEvent = self.curr_mouse_move
         self.currGraphicsView.mouseReleaseEvent = self.curr_mouse_release
         
+        self.overlay = Overlay()
+                
     def scene2image(self, pixmap, image_path):
         img = Image.open(image_path)
         img_size = img.size
@@ -205,6 +262,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         elif event.button() == Qt.RightButton:
             self.clean_bbox(self.prevScene, self.prevXMLPath)
             
+        self.status_of_ob_isChecked(self.prev_image_list)
+        qlist_for_display = self.read_from_dat('store.dat')
+        prev_image_qlist = QStringListModel(qlist_for_display)
+        self.fileListView.setModel(prev_image_qlist)
+            
     def curr_mouse_release(self, event):
         if event.button() == Qt.LeftButton:
             self.rubberBand.hide()
@@ -220,18 +282,57 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         elif event.button() == Qt.RightButton: 
             self.clean_bbox(self.currScene, self.currXMLPath)
         
+        self.status_of_ob_isChecked(self.prev_image_list)
+        qlist_for_display = self.read_from_dat('store.dat')
+        prev_image_qlist = QStringListModel(qlist_for_display)
+        self.fileListView.setModel(prev_image_qlist)
+        
     def image_show_gview(self, index):
+        self.overlay.show()
         prevImgPath = os.path.join(self.file_dir, 'prev', self.prev_image_list[index])
         self.prevImgPath = QDir.toNativeSeparators(prevImgPath)
         currImgPath = os.path.join(self.file_dir, 'curr', self.curr_image_list[index])
         self.currImgPath = QDir.toNativeSeparators(currImgPath)
+        
         self.prevGraphicsView.setStyleSheet("border-image: url(%s);" % self.prevImgPath)
         self.currGraphicsView.setStyleSheet("border-image: url(%s);" % self.currImgPath)
         
         self.fileNameLineEdit.setText(self.curr_image_list[index])
-    
+        self.timer_flag = 1
+        
+    def status_of_ob_isChecked(self, list_of_image):
+        temp_output = open('store.dat', 'w')
+        if list_of_image is not None:
+            for each in list_of_image:
+                file_each = each[:-3] + 'xml'
+                prev_xml_check_path = os.path.join(self.file_dir, 'ob_prev', file_each )
+                curr_xml_check_path = os.path.join(self.file_dir, 'ob_curr', file_each)
+                if os.path.exists(prev_xml_check_path) is True:
+                    if object_isCheck(prev_xml_check_path) is True and object_isCheck(curr_xml_check_path) is True:
+                        w = each + ' ✓' + ' ✓' + '\r\n'
+                    elif object_isCheck(prev_xml_check_path) is False and object_isCheck(curr_xml_check_path) is True:
+                        w = each + ' -' + ' ✓' + '\r\n'
+                    elif object_isCheck(prev_xml_check_path) is True and object_isCheck(curr_xml_check_path) is False:
+                        w = each + ' ✓' + ' -' + '\r\n'
+                    elif object_isCheck(prev_xml_check_path) is False and object_isCheck(curr_xml_check_path) is False:
+                        w = each + ' -' + ' -' + '\r\n'
+                    temp_output.write(w)
+                else:
+                    pass
+        else:
+            print ('load image first')
+        temp_output.close()
+        
+    def read_from_dat(self, dat_file_name):
+        dat_input = open(dat_file_name, 'r')
+        list_of_file_name = []
+        for each in dat_input:
+            list_of_file_name.append(each.strip())
+        dat_input.close()
+        return list_of_file_name
+
     @pyqtSlot()
-    def on_fileNameLineEdit_returnPressed(self):    
+    def on_fileNameLineEdit_returnPressed(self):
         fileName = self.fileNameLineEdit.text()
         if fileName is "":
             print ('Please enter an image name!')
@@ -295,6 +396,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             
             self.prev_count_mouse_click = False
             self.curr_count_mouse_click = False
+            
+            self.status_of_ob_isChecked(self.prev_image_list)
+            qlist_for_display = self.read_from_dat('store.dat')
+            prev_image_qlist = QStringListModel(qlist_for_display)
+            self.fileListView.setModel(prev_image_qlist)
     
     def info_from_xml(self, xml_path, text_edit):
         text_edit.clear()
@@ -344,6 +450,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.draw_init_gview(self.prevXMLPath, self.prevGraphicsView, self.prevScene, Qt.green, self.prevImgPath)
         self.draw_init_gview(self.currXMLPath, self.currGraphicsView, self.currScene, Qt.red, self.currImgPath)
 
+    @pyqtSlot(QModelIndex)
+    def on_fileListView_clicked(self, index):
+        sel_file_name = index.data()
+        self.num_of_index = self.curr_image_list.index(sel_file_name[:-4].strip())
+        self.image_show_gview(self.num_of_index)
+        self.prevScene.clear()
+        self.currScene.clear()
+            
+        self.prevXMLPath = os.path.join(self.file_dir, 'ob_prev', self.prev_xml_list[self.num_of_index])
+        self.currXMLPath = os.path.join(self.file_dir, 'ob_curr', self.prev_xml_list[self.num_of_index])
+        
+        self.info_from_xml(self.prevXMLPath, self.prevPlainTextEdit)
+        self.info_from_xml(self.currXMLPath, self.currPlainTextEdit)
+        
+        self.draw_init_gview(self.prevXMLPath, self.prevGraphicsView, self.prevScene, Qt.green, self.prevImgPath)
+        self.draw_init_gview(self.currXMLPath, self.currGraphicsView, self.currScene, Qt.red, self.currImgPath)
+
     def init_xml_file(self):
         os.makedirs(os.path.join(self.file_dir, 'ob_prev'))
         os.makedirs(os.path.join(self.file_dir, 'ob_curr'))
@@ -353,11 +476,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             curr_info_from_exif = get_info_from_image(os.path.join(self.file_dir, 'curr', f))
             xml_generator(f, 'prev', prev_info_from_exif, self.file_dir)
             xml_generator(f, 'curr', curr_info_from_exif, self.file_dir)
-            
+        
+    def resizeEvent(self, event):
+        self.overlay.resize(event.size())
+        event.accept()
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     ui = MainWindow()
-    ui.showMaximized()
+    #ui.showMaximized()
+    ui.show()
     sys.exit(app.exec_())
-    
-
+   
